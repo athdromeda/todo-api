5e18800075d79c791c62165d9dd5abbaee9452d5
athdromeda <anixatho01@gmail.com>
2024-03-03 16:52:36 +0700

    feat: add route for auth and todo

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::get('data', [AuthController::class, 'data']);
+    });
+});
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('todo', [TodoController::class, 'index']);
+    Route::post('todo', [TodoController::class, 'create']);
+    Route::delete('todo', [TodoController::class, 'delete']);
+    Route::put('todo/{id}', [TodoController::class, 'update']);
 });
